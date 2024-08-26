89db6abd9ebf98f4c3907bd303b13ee2d7e25d5b
Diwakar <dwkrdahal@gmail.com.com>
2024-08-26 09:36:38 +0545

    issue1 fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import FeatureCard from "./feature-card/feature-card";
 import ServiceCard from "./service-card/service-card";
 import Button from "./common/button/button"
-import Header from "./common/header/Header";
+import Header from "./common/header/header";
 import Footer from "./common/footer/footer"
 import BreadCrumb from "./common/breadcrumb/breadcrumb";
 
